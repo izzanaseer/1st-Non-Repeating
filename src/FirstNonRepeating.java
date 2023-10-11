f40ac151aabdef52503cea10dc1b32eafd8b36d8
izzanaseer <izzanaseer3@gmail.com>
2023-10-11 22:23:37 +0500

    Version 2

--- src/FirstNonRepeating.java
+++ src/FirstNonRepeating.java
@@ -2,20 +2,54 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FirstNonRepeating {
-    private Map<Integer, Object> address;
-    private Map<Integer, Boolean> repeated;
+    private Map<Character, Node> characters;
+    private Node head;
+    private Node tail;
+    private class Node {
+        char data;
+        Node prev;
+        Node next;
 
-    public FirstNonRepeating() {
-        address = new HashMap<>();
-        repeated = new HashMap<>();
-
-        for (int i = 0; i < 26; i++) {
-            address.put(i, null);
-            repeated.put(i, false);
+        Node(char data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
         }
     }
 
-    public void add(char c){
-        int index = c - 'a' ;
+    public FirstNonRepeating() {
+        characters = new HashMap<>();
+        head = new Node('0');
+        tail = new Node('0');
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public void add(char c) {
+        if (!characters.containsKey(c))
+        {
+            Node nn = new Node(c);
+            nn.prev = tail.prev;
+            nn.next = tail;
+            tail.prev.next = nn;
+            tail.prev = nn;
+            characters.put(c, nn);
+        }
+        else if (characters.get(c) != null)
+        {
+            Node repeatedNn = characters.get(c);
+            characters.put(c, null);
+            repeatedNn.prev.next = repeatedNn.next;
+            repeatedNn.next.prev = repeatedNn.prev;
+        }
+    }
+
+    public char getFirstNonRepeating() {
+        if (head.next == tail) {
+            return '-';
+        } else {
+            return head.next.data;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        FirstNonRepeating stream = new FirstNonRepeating();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type '/' to exit");
+        System.out.println("Stream: ");
+        char userInput;
+
+        do {
+            userInput = scanner.next().charAt(0);
+            if (userInput == '/') {
+                break;
+            }
+
+            stream.add(userInput);
+        } while (true);
+
+        System.out.print("First Non Repeating Character = " + stream.getFirstNonRepeating());
     }
 }
